91c13bcc2426ef72356b96451881826fa370e832
AlexandrIvanovYdomlya <ivanovsunsunich@yandex.com>
2023-02-17 21:33:43 +0300

    first commit

---
+++ src/components/Product.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export function Product() {
+    return (
+        <div
+        className="border py-2 rounded flex flex-col items-center mb-2">Product!</div>
+    )
+}
