8bb75ff7884f52d70bbc9be1b02472d187ed3dca
AlexandrIvanovYdomlya <ivanovsunsunich@yandex.com>
2023-02-26 00:08:01 +0300

    wrthwrythwtt

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export function Product(props:any) {
-    console.log(props)
     return (
         <div
         className="border py-2 rounded flex flex-col items-center mb-2"
